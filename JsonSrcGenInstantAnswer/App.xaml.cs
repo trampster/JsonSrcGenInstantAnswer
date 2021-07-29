760f64deea6ed608af8d32b8ca951133144477d8
trampster <trampster@gmail.com>
2021-07-29 23:37:44 +1200

    Add MainWindows with search box.

---
+++ JsonSrcGenInstantAnswer/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using JsonSrcGenInstantAnswer.ViewModels;
+using System.Windows;
+
+namespace JsonSrcGenInstantAnswer
+{
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application
+   {
+      protected override void OnStartup(StartupEventArgs e)
+      {
+         base.OnStartup(e);
+
+         MainWindow = new MainWindow(new SearchViewModel());
+         MainWindow.Show();
+      }
+   }
+}
---
+++ JsonSrcGenInstantAnswer/ViewModels/SearchViewModel.cs
@@ -0,0 +1,64 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace JsonSrcGenInstantAnswer.ViewModels
+{
+   public class SearchViewModel : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      string _searchText = "";
+      public string SearchText
+      {
+         get => _searchText;
+         set
+         {
+            SetProperty(ref _searchText, value);
+            UpdateImageColors();
+         }
+      }
+
+      void UpdateImageColors()
+      {
+         if(SearchText.Length > 0)
+         {
+            SearchButtonForegroundColor = _whiteColorBrush;
+            SearchButtonBackgroundColor = _yellowColorBrush;
+            return;
+         }
+         SearchButtonForegroundColor = _grayColorBrush;
+         SearchButtonBackgroundColor = _transparentColorBrush;
+      }
+
+      static readonly SolidColorBrush _grayColorBrush = new SolidColorBrush(Colors.Gray);
+      static readonly SolidColorBrush _transparentColorBrush = new SolidColorBrush(Colors.Transparent);
+      static readonly SolidColorBrush _yellowColorBrush = new SolidColorBrush(Color.FromRgb(0x63, 0xad, 0x5f));
+      static readonly SolidColorBrush _whiteColorBrush = new SolidColorBrush(Colors.White);
+
+      SolidColorBrush _searchButtonForegroundColor = _grayColorBrush;
+      public SolidColorBrush SearchButtonForegroundColor
+      {
+         get => _searchButtonForegroundColor;
+         set => SetProperty(ref _searchButtonForegroundColor, value);
+      }
+
+      SolidColorBrush _searchButtonBackgroundColor = _transparentColorBrush;
+      public SolidColorBrush SearchButtonBackgroundColor
+      {
+         get => _searchButtonBackgroundColor;
+         set => SetProperty(ref _searchButtonBackgroundColor, value);
+      }
+
+      void SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
+      {
+         if(backingStore.Equals(value))
+         {
+            return;
+         }
+         backingStore = value;
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+   }
+}
